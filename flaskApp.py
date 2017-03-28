26ed2bc9a0f6eab2611f125db51d719065f93c98
ggersh <gershkowitz.3@osu.edu>
2017-03-27 21:25:52 -0400

    run times

--- flaskApp.py
+++ flaskApp.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, url_for, redirect, flash,  g,
 import requests
 import json
 from fusionAPI import query_api
+from tester import findTimeTest
 
 # pagination extension since yelp api doesn't directly offer pagination
 from flask_paginate import Pagination, get_page_args
@@ -17,33 +18,38 @@ def index():
 
 @app.route('/search', methods=['GET','POST'])
 def my_form_post():
-	search = False
-	PER_PAGE = 10
-	if request.method == 'POST':
-		print('***in form***')
-		term = request.form['term']
-		location = request.form['location']
-		price = request.form['price']
-		open_now = request.form['open']
-		# print(term, location, price, open_now)
-		result = query_api(term=term, location=location,price=price,open_now=open_now)
-		if not result:
-			result = []
-		# cache this result so we can paginate through it
-		cache['searches'] = result
-		cache['location'] = location
-		cache['term'] = term
-	if 'searches' not in cache:
-		# user reloads search page
-		return redirect(url_for('index'))
-	page = request.args.get('page', type=int, default=1)
-	start_index = (page-1) * PER_PAGE
-	end_index = start_index + PER_PAGE
-	page_data = cache['searches'][start_index:end_index]
-	location = cache['location']
-	term = cache['term']
-	pagination = Pagination(css_framework='bootstrap3', page=page, per_page=PER_PAGE, total=len(cache['searches']), search=search, record_name='searches')
-	return render_template('results.html', location = location, term = term, data=page_data, pagination=pagination)
+    search = False
+    PER_PAGE = 10
+
+    if request.method == 'POST':
+        print('***in form***')
+        term = request.form['term']
+        location = request.form['location']
+        price = request.form['price']
+        open_now = request.form['open']
+        time = request.form['time']
+    	# print(term, location, price, open_now)
+        result = query_api(term=term, location=location,price=price,open_now=open_now)
+        run_times = findTimeTest(time = time, data = result)
+        if not result:
+    		result = []
+    	# cache this result so we can paginate through it
+        cache['searches'] = result 
+        cache['location'] = location
+        cache['term'] = term
+        cache['run_times'] = run_times
+    if 'searches' not in cache:
+    	# user reloads search page
+    	return redirect(url_for('index'))
+    page = request.args.get('page', type=int, default=1)
+    start_index = (page-1) * PER_PAGE
+    end_index = start_index + PER_PAGE
+    page_data = cache['searches'][start_index:end_index]
+    location = cache['location']
+    term = cache['term']
+    run_times = cache['run_times'][start_index:end_index]
+    pagination = Pagination(css_framework='bootstrap3', page=page, per_page=PER_PAGE, total=len(cache['searches']), search=search, record_name='searches')
+    return render_template('results.html', location = location, term = term, data=page_data, pagination=pagination, run_times=run_times)
 
 	# print(json.dumps(searches, sort_keys = False, indent = 2))
 
---
+++ tester.py
@@ -0,0 +1,33 @@
+import time
+import json
+def findTimeTest(time, data):
+	timeToRun = []
+	seconds = convertToSeconds(time)
+	##text=List of strings to be written to file
+	for i in data:
+		# with open('csvfile.csv','wb') as file:
+		# 	file.write(str(i.get('distance')))
+		secondsTotal = i.get('distance')/(1609.34/seconds)
+		hours, sec =  secondsTotal // 3600, secondsTotal % 3600
+		min, sec = sec // 60, sec % 60
+	 	if (min == 0):
+			time = str(int(sec))
+		elif (hours == 0):
+			time = str(int(min))+'m:'+str(int(sec))+"s"
+		else:
+			time = str(int(hours))+'h:'+str(int(min))+'m:'+str(int(sec))+"s"
+		timeToRun.append(time)
+	return timeToRun
+
+def convertToSeconds(time):
+	min, sec = time.split(':')
+	return int(min) * 60 + int(sec)
+
+# def main():
+# 	t = "6:08"
+# 	d = 5000
+# 	a = findTimeTest(t, d)
+# 	print a
+#
+# if __name__ == '__main__':
+#     main()
