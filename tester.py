bdb453c65b0314584de3798486747d5b134dcdba
ggersh <gershkowitz.3@osu.edu>
2017-03-27 23:33:38 -0400

    fixed times

--- tester.py
+++ tester.py
@@ -10,7 +10,7 @@ def findTimeTest(time, data):
 		secondsTotal = i.get('distance')/(1609.34/seconds)
 		hours, sec =  secondsTotal // 3600, secondsTotal % 3600
 		min, sec = sec // 60, sec % 60
-	 	if (min == 0):
+	 	if (min == 0 and hours == 0):
 			time = str(int(sec))+"s"
 		elif (hours == 0):
 			time = str(int(min))+'m:'+str(int(sec))+"s"
